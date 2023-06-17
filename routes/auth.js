a1b012bc804785b2a6f83ae46434e590d642623e
lielca <liel28@gmail.com>
2023-06-17 15:41:07 +0300

    updates

--- routes/auth.js
+++ routes/auth.js
@@ -66,8 +66,8 @@ router.post("/Login", async (req, res, next) => {
 
     // Set cookie
     req.session.user_id = user.user_id;
-
-
+    console.log("login"+req.session.user_id)
+    
     // return cookie
     res.status(200).send({ message: "login succeeded", success: true });
 
